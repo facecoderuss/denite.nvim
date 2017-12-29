25949341f66b54d89a61cb86b3cb5eed11c66bb0
Антон Денисенко <facecoderuss@ya.ru>
2017-12-29 15:33:16 +0300

    Update __init__.py

--- rplugin/python3/denite/__init__.py
+++ rplugin/python3/denite/__init__.py
@@ -1,29 +1,27 @@
 # =============================================================================
 # FILE: denite.py
-# AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
 # License: MIT license
 # =============================================================================
 
 import traceback
 from importlib import find_loader
 
-
 if not find_loader('vim'):
     import neovim
     from denite.ui.default import Default
 
-    @neovim.plugin
+@neovim.plugin
     class DeniteHandlers(object):
         def __init__(self, vim):
             self._vim = vim
 
-        @neovim.function('_denite_init', sync=True)
+@neovim.function('_denite_init', sync=True)
         def init_python(self, args):
             self._uis = {}
             self._vim.vars['denite#_channel_id'] = self._vim.channel_id
             return
 
-        @neovim.function('_denite_start', sync=True)
+@neovim.function('_denite_start', sync=True)
         def start(self, args):
             from denite.util import error
             try:
